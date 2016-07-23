46d0e4ce48fce082f7c4503d26ae2f798e05a416
jhjgwlgzh <114350473@qq.com>
2016-07-23 09:04:32 +0800

    Main调用Seccond成功！

--- app/src/main/java/com/jhj/activitylifecycle/ActivitySecond.java
+++ app/src/main/java/com/jhj/activitylifecycle/ActivitySecond.java
@@ -1,9 +1,18 @@
 package com.jhj.activitylifecycle;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by lsx on 2016/7/22.
  */
 public class ActivitySecond extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String infom = getIntent().getStringExtra("name");
+        Toast.makeText(ActivitySecond.this, infom, Toast.LENGTH_SHORT).show();
+    }
 }
